63a7ab1ff50f7c33ee790cba1a1300f443b51aa9
Rodrigo Oliveira <fulanodasilva.git@gmail.com>
2021-03-17 16:36:42 -0300

    finalização das preparações Pre-javascript

--- js/script.js
+++ js/script.js
@@ -1,11 +1,49 @@
-const modalElemento = document.querySelector('.modal-overlay')
-
+// Tentar Encontrar outro metodo para adicionar class
 const Modal = {
 	open(){
-		modalElemento.classList.add('active')
+	   document
+		.querySelector('.modal-overlay')
+		.classList
+		.add('active')
 		},
 	close(){
-		modalElemento.classList.remove('active')
+	   document
+		.querySelector('.modal-overlay')
+		.classList
+		.remove('active')
+	}
+}
+
+const transactions = [
+{
+	id: 1,
+	description:'Luz',
+	amount: -50000,
+	date:'23/01/2021'
+},
+{
+	id: 2,
+	description:'Criação de website',
+	amount: 500000,
+	date:'23/01/2021'
+},
+{
+	id: 1,
+	description:'Internet',
+	amount: -20000,
+	date:'23/01/2021'
+}
+]
+
+const Transaction = {
+	incomes(){
+		//somar as entradas
+	},
+	expenses(){
+		//somar as saidas
+	},
+	total(){
+		//entradas - saidas
 	}
 }
 
